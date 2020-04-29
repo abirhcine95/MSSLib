eb0836cb31985558d2d9f821f5e01add92d2654a
Abir Hcine <abir.hcine@mssolutions-group.com>
2020-04-29 09:49:18 +0100

    Initial Commit

---
+++ MSSTestLib/MSSTestLib.h
@@ -0,0 +1,19 @@
+//
+//  MSSTestLib.h
+//  MSSTestLib
+//
+//  Created by mac on 29/04/2020.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MSSTestLib.
+FOUNDATION_EXPORT double MSSTestLibVersionNumber;
+
+//! Project version string for MSSTestLib.
+FOUNDATION_EXPORT const unsigned char MSSTestLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSSTestLib/PublicHeader.h>
+
+
